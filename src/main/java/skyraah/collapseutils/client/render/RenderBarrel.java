6f317198633dab3337023d3f50d435e60b850142
cyciling <1377899390@qq.com>
2021-06-28 17:59:54 +0800

    Update RenderBarrel.java

---
+++ src/main/java/skyraah/collapseutils/client/render/RenderBarrel.java
@@ -0,0 +1,13 @@
+package skyraah.collapseutils.client.render;
+
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraftforge.client.model.animation.FastTESR;
+import skyraah.collapseutils.block.tileentity.TileEntityBarrel;
+
+public class RenderBarrel extends FastTESR<TileEntityBarrel> {
+
+    @Override
+    public void renderTileEntityFast(TileEntityBarrel te, double x, double y, double z, float partialTicks, int destroyStage, float partial, BufferBuilder buffer) {
+
+    }
+}
